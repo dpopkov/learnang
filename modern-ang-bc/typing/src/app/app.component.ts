75d2bd4390aff9636d6032470c5882ce490444b3
Denis Popkov <pkvdenis@gmail.com>
2020-11-03 20:05:14 +0300

    modernangbc: typing: exercise - implement typing game.

--- modern-ang-bc/typing/src/app/app.component.ts
+++ modern-ang-bc/typing/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Typing Challenge';
+  randomText: string = lorem.sentence();
+  enteredText: string = '';
+  solved: boolean = false;
+
+  onInput(typedText: string): void {
+    this.enteredText = typedText;
+    this.solved = typedText === this.randomText;
+  }
+
+  classForLetterAt(idx: number): string {
+    let styleClass: string = '';
+    if (idx >= this.enteredText.length) {
+      styleClass = 'pending';
+    } else {
+      styleClass = (this.randomText[idx] === this.enteredText[idx]) ? 'correct' : 'incorrect'
+    }
+    return styleClass;
+  }
 }
