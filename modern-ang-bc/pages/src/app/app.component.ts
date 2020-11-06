a3221a7b22ac1613b888b885990a4c49474c8b0c
Denis Popkov <pkvdenis@gmail.com>
2020-11-06 18:16:04 +0300

    modernangbc: pages: start implementing pagination.

--- modern-ang-bc/pages/src/app/app.component.ts
+++ modern-ang-bc/pages/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage: number = 0;
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    },
+    {
+      title: 'Boardwalk',
+      url: 'https://images.unsplash.com/photo-1495954484750-af469f2f9be5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    },
+    {
+      title: 'Coast',
+      url: 'https://images.unsplash.com/photo-1527437934671-61474b530017?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    },
+    {
+      title: 'Bridge',
+      url: 'https://images.unsplash.com/photo-1473221326025-9183b464bb7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80'
+    }
+  ];
 }
