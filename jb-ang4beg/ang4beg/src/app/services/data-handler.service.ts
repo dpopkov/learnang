4729700527083ce5ed41a621124409f3b6c151be
Denis Popkov <pkvdenis@gmail.com>
2021-02-17 21:28:08 +0300

    ang4beg: implement event handling (show Tasks by Category) in reactive style.

--- jb-ang4beg/ang4beg/src/app/services/data-handler.service.ts
+++ jb-ang4beg/ang4beg/src/app/services/data-handler.service.ts
@@ -2,26 +2,29 @@ import {Injectable} from '@angular/core';
 import {Category} from "../model/Category";
 import {TestData} from "../data/TestData";
 import {Task} from "../model/Task";
+import {BehaviorSubject, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
+  taskSubject = new BehaviorSubject<Task[]>(TestData.tasks);
+  categorySubject = new BehaviorSubject<Category[]>(TestData.categories);
+
   constructor() {
   }
 
-  getCategories(): Category[] {
-    return TestData.categories;
+  /*fillCategories() {
+    this.categorySubject.next(TestData.categories);
+  }*/
+
+  fillTasks() {
+    this.taskSubject.next(TestData.tasks);
   }
 
-  getTasks(): Task[] {
-    return TestData.tasks;
-  }
-
-  getTasksByCategory(category: Category): Task[] {
-    let tasks = TestData.tasks.filter(c => c.category === category);
-    console.log(tasks);
-    return tasks;
+  fillTasksByCategory(category: Category) {
+    const tasks = TestData.tasks.filter(c => c.category === category);
+    this.taskSubject.next(tasks);
   }
 }
--- jb-ang4beg/ang4beg/src/app/views/tasks/tasks.component.ts
+++ jb-ang4beg/ang4beg/src/app/views/tasks/tasks.component.ts
@@ -15,7 +15,6 @@ export class TasksComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.tasks = this.dataHandler.getTasks();
+    this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
   }
-
 }
