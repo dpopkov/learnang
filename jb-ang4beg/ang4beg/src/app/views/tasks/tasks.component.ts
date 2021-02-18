d3001100a98871075ef027ba510fd3884d5d3fb4
Denis Popkov <pkvdenis@gmail.com>
2021-02-18 16:03:29 +0300

    ang4beg: add style and handle click on Status of Task.

--- jb-ang4beg/ang4beg/src/app/views/tasks/tasks.component.ts
+++ jb-ang4beg/ang4beg/src/app/views/tasks/tasks.component.ts
@@ -17,4 +17,8 @@ export class TasksComponent implements OnInit {
   ngOnInit(): void {
     this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
   }
+
+  toggleTaskCompleted(task: Task) {
+    task.completed = !task.completed;
+  }
 }
